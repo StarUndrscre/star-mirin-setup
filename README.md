f2753fc065a952c69160b05eb0553c88d026e896
StarUndrscre <davidtigasgameup@gmail.com>
2022-04-20 22:20:40 -0700

    Upload files

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# star-mirin-setup
-files that i usually put in my own modfiles
+# StarUndrscre's Mirin Template Setup
+A collection of files that I usually include in my modfile releases
+
+Includes:
+* Manipulable background
+* Definemods (centering playfields, bg mods (moving, rotating, etc), other stuff)
---
+++ lua/definemods.lua
@@ -0,0 +1,55 @@
+-- StarUndrscre's Definemods v0.1
+
+local p1DefaultX = P1:GetX()
+local p2DefaultX = P2:GetX()
+local defaultZ = P1:GetZ()
+definemod { 'centerPlayfields', function(percent) 
+    local scale = percent / 100
+    P1:x((scale*scx) + ((1-scale)*p1DefaultX))
+    P2:x((scale*scx) + ((1-scale)*p2DefaultX))
+end }
+
+definemod {'speen', function(percent)
+    local p = percent/100
+    local confusionyoffset = (math.rad(-360)*100)*p
+    local rotationy = 360*p
+    return confusionyoffset, rotationy
+end, 'confusionyoffset', 'rotationy'}
+
+definemod {'bgzoom', function(percent)
+    local actualPercent = percent / 100
+    bg:zoom(actualPercent)
+end}
+
+definemod {'bgrotatex', function(percent)
+    bg:rotationx(percent)
+end}
+
+definemod {'bgrotatey', function(percent)
+    bg:rotationy(percent)
+end}
+
+definemod {'bgrotatez', function(percent)
+    bg:rotationz(percent)
+end}
+
+definemod {'bgmovex', function(percent)
+    bg:x(SCREEN_CENTER_X+percent)
+end}
+
+definemod {'bgmovey', function(percent)
+    bg:y(SCREEN_CENTER_Y+percent)
+end}
+
+definemod {'bgmovez', function(percent)
+    bg:z(percent)
+end}
+
+definemod {'bgalpha', function(percent)
+    local actualPercent = percent / 100
+    bg:glow(0, 0, 0, actualPercent)
+end}
+
+setdefault {100, 'bgzoom', 40, 'bgalpha'}
+
+-- put other definemods here:
